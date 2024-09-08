13c2a865668d0b91aadca41de0f68a16b9fbc74f
Lincoln Dias <lincolnroberto820@gmail.com>
2024-09-08 12:37:06 -0300

    Projeto criado

---
+++ src/test/java/com/link1nk/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.link1nk.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
