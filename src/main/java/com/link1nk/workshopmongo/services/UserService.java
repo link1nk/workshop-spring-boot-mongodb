f16c66d36a999b8e4a75f391dea6844e3758d5e6
Lincoln Dias <lincolnroberto820@gmail.com>
2024-09-08 17:04:13 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/link1nk/workshopmongo/services/UserService.java
+++ src/main/java/com/link1nk/workshopmongo/services/UserService.java
@@ -32,6 +32,11 @@ public class UserService {
 		return userRepository.insert(user);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO user) {
 		return new User(user.getId(), user.getName(), user.getEmail());
 	}
